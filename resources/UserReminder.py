2b660786b576f126e492c05d02a3a5901e5f6461
Jordy Shryock <jordy1611@gmail.com>
2020-10-26 13:20:22 -0600

    Change schema in user reminder to reminder schema

--- resources/UserReminder.py
+++ resources/UserReminder.py
@@ -1,12 +1,12 @@
 from flask import jsonify, request
 from flask_restful import Resource
-from Model import db, User, Reminder, UserSchema
+from Model import db, User, Reminder, ReminderSchema
 
-users_schema = UserSchema(many=True)
-user_schema = UserSchema()
+reminder_schema = ReminderSchema(many=True)
+
 
 class UserReminder(Resource):
     def get(self, user_id):
         reminders = Reminder.query.filter_by(user_id = user_id)
-        reminders = users_schema.dump(reminders)
+        reminders = reminder_schema.dump(reminders)
         return {"status":"success", "data":reminders}, 200
