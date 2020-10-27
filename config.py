ac27aa97cd53a0e38b3826b453560539624da863
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-10-26 18:49:03 -0600

    Set DATABASE_URL as os.environment

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres@localhost:5432/mediminderx_be"
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres@localhost:5432/mediminderx_be')
