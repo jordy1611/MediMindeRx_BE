35c26a54f15ae84c727a16b8de3e7b71c4e19670
Kathy Bui <kathy.bui1992@gmail.com>
2020-10-26 21:54:03 -0600

    Update user resource

--- resources/User.py
+++ resources/User.py
@@ -9,54 +9,46 @@ class UserResource(Resource):
     def get(self):
         users = User.query.all()
         users = users_schema.dump(users)
-        return {"status":"success", "data":users}, 200
+        return users, 200
 
     def post(self):
         json_data = request.get_json(force=True)
-    #     if not json_data:
-    #         return {'message': 'No input data provided'}, 400
-    # # Validate and deserialize input
-    #     data, errors = user_schema.load(json_data)
-    #     if errors:
-    #         return errors, 422
-    #         user = User.query.filter_by(name=data['name']).first()
-    #     if user:
-    #         return {'message': 'User already exists'}, 400
+        user = User.query.filter_by(name=json_data['name']).first()
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        elif user:
+            return {'message': 'User already exists'}, 400
 
         user = User(name=json_data['name'])
 
         db.session.add(user)
         db.session.commit()
         result = user_schema.dump(user)
-        return { "status": 'success', 'data': result }, 201
+        return result, 201
 
     def put(self):
         json_data = request.get_json(force=True)
-    #     if not json_data:
-    #         return {'message': 'No input data provided'}, 400
-    # # Validate and deserialize input
-    #     data, errors = user_schema.load(json_data)
-    #     if errors:
-    #         return errors, 422
-    #         user = User.query.filter_by(id=data['id']).first()
-    #     if not user:
-    #         return {'message': 'User does not exist'}, 400
+        user = User.query.filter_by(id=json_data['id']).first()
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        elif not user:
+            return {'message': 'User does not exist'}, 400
 
+        user = User.query.filter_by(id=json_data['id']).first()
         user.name = json_data['name']
         db.session.commit()
         result = user_schema.dump(user)
-        return { "status": 'success', 'data': result }, 200
+        return result, 200
 
     def delete(self):
         json_data = request.get_json(force=True)
-    #     if not json_data:
-    #         return {'message': 'No input data provided'}, 400
-    # # Validate and deserialize input
-    #     data, errors = user_schema.load(json_data)
-    #     if errors:
-    #         return errors, 422
+        user = User.query.filter_by(id=json_data['id']).first()
+        if not json_data:
+            return {'message': 'No input data provided'}, 400
+        elif not user:
+            return {'message': 'User does not exist'}, 400
 
         user = User.query.filter_by(id=json_data['id']).delete()
         db.session.commit()
         result = user_schema.dump(user)
-        return { "status": 'success', 'data': 'User has been successfully deleted'}, 200
+        return { 'message': 'User has been successfully deleted'}, 200
