239b6b0bc04b732d25b8be46a376de54e26047f1
Kathy Bui <kathy.bui1992@gmail.com>
2020-10-26 23:16:50 -0600

    Update files

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from resources.Location import LocationResource
 from resources.Scheduled import ScheduledResource
 from resources.UserId import UserIdResource
 from resources.UsersReminder import UsersReminderResource
+from flask import Flask
+
+app = Flask(__name__)
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -17,3 +20,6 @@ api.add_resource(LocationResource, '/locations')
 api.add_resource(ScheduledResource, '/scheduled')
 api.add_resource(UserIdResource, '/users/<int:id>')
 api.add_resource(UsersReminderResource, '/users/<int:user_id>/reminders')
+
+if __name__ == "__main__":
+    app.run(debug=True)
