1bcdac32e1c939080c1f4f3effc07ca7884148c8
Kathy Bui <kathy.bui1992@gmail.com>
2020-10-26 21:51:28 -0600

    Update Routes

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from resources.Reminder import ReminderResource
 from resources.User import UserResource
-from resources.UserId import UserId
-from resources.UserReminder import UserReminder
+from resources.Location import LocationResource
+from resources.Scheduled import ScheduledResource
+from resources.UserId import UserIdResource
+from resources.UsersReminder import UsersReminderResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
-# Route
-# api.add_resource(Hello, '/hello', '/hello/<int:id>')
+# Routes
 api.add_resource(ReminderResource, '/reminders')
 api.add_resource(UserResource, '/users')
-api.add_resource(UserId, '/users/<int:id>')
-api.add_resource(UserReminder, '/users/<int:user_id>/reminders')
-
+api.add_resource(LocationResource, '/locations')
+api.add_resource(ScheduledResource, '/scheduled')
+api.add_resource(UserIdResource, '/users/<int:id>')
+api.add_resource(UsersReminderResource, '/users/<int:user_id>/reminders')
